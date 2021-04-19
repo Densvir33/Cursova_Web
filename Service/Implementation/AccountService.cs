5adbdee6109c3e93f5abe369fceade440c691758
Ernendo <densvir33@gmail.com>
2021-04-19 18:02:25 +0300

    Register/Login

---
+++ Service/Implementation/AccountService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Models.DTO.IdentityDTO;
+using Models.DTO.ResultDTO;
+using Models.Entities;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Implementation
+{
+    class AccountService : IAccountService
+    {
+
+        private readonly ApplicationContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IJwtTokenService _jwtTokenService;
+        public AccountService( ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public Task<CollectionResultDTO<LoginDTO>> Login(LoginDTO user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CollectionResultDTO<RegisterDTO>> Register(RegisterDTO newUser)
+        {
+            User user = new User()
+            {
+                Email = newUser.Email,
+                PhoneNumber = newUser.PhoneNumber,
+                UserName = newUser.Email
+            };
+            await _userManager.CreateAsync(user, newUser.Password);
+            UserInfo ui = new UserInfo()
+            {
+                Id = user.Id,
+                FullName = newUser.UserName
+            };
+            await _context.UserInfos.AddAsync(ui);
+            await _context.SaveChangesAsync();
+
+            return new CollectionResultDTO<RegisterDTO>
+            {
+                IsSuccessful = true
+            };
+        }
+
+
+
+
+
+    }
+}
---
+++ Store/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTO.IdentityDTO;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO user)
+        {
+            return Ok(await _accountService.Register(user));
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO user)
+        {
+            return Ok(await _accountService.Login(user));
+        }
+
+    }
+}
